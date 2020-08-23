74287ee1868fa13be6ba4a4dbac04bdcd70d9cdc
Peter Bona <bonapeti@gmail.com>
2020-08-24 01:39:27 +0200

    Add selection sort

--- sort.py
+++ sort.py
@@ -1,5 +1,6 @@
 from timeit import default_timer as timer
 import pytest
+import sys
 
 def bubble_sort(array):
     if array is None:
@@ -27,12 +28,22 @@ def insertion_sort(array):
 
 def selection_sort(array):
 
-
+    def find_smallest_element(array):
+        smallest_so_far = sys.maxint
+        smallest_index = -1
+        for index, value in enumerate(array):
+            if value < smallest_so_far:
+                smallest_so_far = value
+                smallest_index = index
+        return smallest_index, smallest_so_far
 
     for separator_index in range(len(array)):
-        smallest, smallest_so_far_index = find_index_of_smallest(array[separator_index:len(array)])
-        array[separator_index -1] 
-        pass
+        smallest_so_far_index, smallest = find_smallest_element(array[separator_index:len(array)])
+        if smallest_so_far_index != -1:
+            for swapping_index in range(smallest_so_far_index,0,-1):
+                real_swapping_index = swapping_index + separator_index
+                array[real_swapping_index] = array[real_swapping_index-1]
+            array[separator_index] = smallest
 
     return array
 
@@ -49,15 +60,17 @@ def ordered_list(size):
                                                     (insertion_sort,[],[]),
                                                     (insertion_sort,[1],[1]),
                                                     (insertion_sort,[3,2,1],[1,2,3]),
-                                                    (selection_sort,[3,2,1],[1,2,3])
+                                                    (selection_sort,[3,2,1],[1,2,3]),
+                                                    (selection_sort,[11, 25, 12, 22, 64],[11,12,22, 25, 64])
                                                     ])
 def test_sort(sort_fn, input, expected):
     assert sort_fn(input) == expected
 
 @pytest.mark.parametrize("sort_fn, sizes", [(bubble_sort, [10,100,1000,10000]), 
-                                            (insertion_sort,[10,100,1000,10000])
+                                            (insertion_sort,[10,100,1000,10000]),
+                                            (selection_sort,[10,100,1000,10000])
                                                     ])
-def te1st_performance(sort_fn, sizes):
+def test_performance(sort_fn, sizes):
     for size in sizes:
         start = timer()
         sort_fn(reversed_list(size))
